0ee27d660a8f82598cccc784072d91d66a5dddd6
Adam Tanweer <113802807+Poltanek@users.noreply.github.com>
2024-08-29 10:04:15 +0100

    update

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.arjuncodes.hello;
+
+import org.springframework.boot.SpringApplication;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        String name = "John";
+        System.out.println("Hello, " + name + "!");
+    }
+}
