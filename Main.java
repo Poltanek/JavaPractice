e45f9ca568ad317e0a83868c16840fcd2dd72891
Adam Tanweer <113802807+Poltanek@users.noreply.github.com>
2024-09-04 17:04:28 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,6 @@ package com.arjuncodes.hello;
 
 import org.springframework.boot.SpringApplication;
 
-
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
