d599de56e184c15b7bd2fabe611b7cf83f54121b
Adam Tanweer <113802807+Poltanek@users.noreply.github.com>
2024-09-16 23:27:24 +0100

    update

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
-package com.arjuncodes.hello;
-
-import org.springframework.boot.SpringApplication;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        String name = "John";
-        System.out.println("Hello, " + name + "!");
+        double amount;
+        amount = 5.95;
+        amount = amount + 25.00;
+        System.out.print("We will bill $");
+        System.out.print(amount);
+        System.out.println(" To your credit card.");
     }
 }
